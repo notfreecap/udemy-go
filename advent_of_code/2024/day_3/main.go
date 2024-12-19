4c0d7cf17a36c5e846ff1848daf71e50b7d11ada
notfreecap <andresrenew@gmail.com>
2024-12-19 18:00:29 -0500

    fix

--- advent_of_code/2024/day_3/main.go
+++ advent_of_code/2024/day_3/main.go
@@ -10,31 +10,52 @@ import (
 )
 
 func main() {
-	data, err := os.ReadFile("data")
+	data, err := os.ReadFile("data2")
 	if err != nil {
 		log.Fatal("Unable to read file", err)
 	}
 
-	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
-
 	sum := 0
 	lines := strings.Split(string(data), "\n")
 
-	for _, line := range lines {
+	for i, line := range lines {
 		if line == "" {
 			continue
 		}
-		matches := re.FindAllStringSubmatch(line, -1)
-		iterateMatches(&sum, matches)
+		isFirstLine := i == 0
+		checkValidity(line, isFirstLine, &sum)
+		// iterateMatches(&sum, line)
 	}
 
 	fmt.Println("total", sum)
 }
 
-func iterateMatches(sum *int, matches [][]string) {
+func iterateMatches(sum *int, line string) {
+	fmt.Println(line)
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	matches := re.FindAllStringSubmatch(line, -1)
 	for _, match := range matches {
 		a, _ := strconv.Atoi(match[1])
 		b, _ := strconv.Atoi(match[2])
 		*sum += a * b
+	}
+}
+
+func checkValidity(line string, isFirstLine bool, sum *int) {
+	re := regexp.MustCompile(`[^d]*(?:do\(\)|'?don't\(\))`)
+	matches := re.FindAllString(line, -1)
+	for i, match := range matches {
+		if i == 0 && isFirstLine {
+			recheck := regexp.MustCompile(`do\(\)|don't\(\)`)
+			result := recheck.Split(match, -1)
+			if len(result) > 0 {
+				iterateMatches(sum, result[0])
+			}
+			continue
+		}
+		if strings.Contains(match, "don't()") {
+			continue
+		}
+		iterateMatches(sum, match)
 	}
 }
